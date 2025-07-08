f85bee83688c065df254aec5221d20c6fad53776
Gowtham Selvaraj <gowthammu47@gmail.com>
2025-07-08 16:04:58 +0530

    Added MySQL configuration and restaurant models

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -76,6 +76,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- LittleLemon/restaurant/admin.py
+++ LittleLemon/restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Menu, Reservation
+
+admin.site.register(Menu)
+admin.site.register(Reservation)
