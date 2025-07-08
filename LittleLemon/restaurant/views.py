afb3e3aea4714a45309e98c086c6c2b3511f48f9
Gowtham Selvaraj <gowthammu47@gmail.com>
2025-07-08 16:15:49 +0530

    "Added DRF support with Menu and Reservation API endpoints"

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Menu, Reservation
+from .serializers import MenuSerializer, ReservationSerializer
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
